4649fd3012574cd3c49b404bc040f57543bd224c
Andrii Hnatyshyn <andrii.hnatyshyn@gmail.com>
2016-04-01 15:00:47 +0300

    013 lesson : problems with packedges and image uploade

--- client/templates/profile/profile.js
+++ client/templates/profile/profile.js
@@ -1,3 +1,27 @@
-/**
- * Created by user on 28.03.2016.
- */
+Template.profile.events({
+	"submit .edit-profile":function(event){
+		var file = $('#profileImage').get(0).files[0];
+		if(file){
+			fsFile = new FS.Filter(file);
+
+			profileImages.insert(fsFile,function(err, result){
+				if(err){
+					throw new Meteor.Error(err);
+				}else{
+
+					var imageLoc = ''+result._id;
+
+					UserImages.insert({
+						userId : Meteor.userId(),
+						username: Meteor.user().username,
+						image: imageLOc
+					});
+
+					Router.go('/');
+				}
+			});
+		}
+		console.log(file);
+		return false;
+	}
+});
